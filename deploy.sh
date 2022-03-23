9bda0b10cc682097a12f1470fd396290f34dfb1e
Evgenii <osipenkov.evgenii@gmail.com>
2022-03-23 12:25:27 +0300

    Add deployment to the github pages script

---
+++ deploy.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env sh
+
+# check .env file
+if [ -f .env ]
+then
+  export $(cat .env | sed 's/#.*//g' | xargs)
+else
+  echo "The file .env not found!"
+  exit 0
+fi
+
+# abort on errors
+# set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:$VUE_APP_USER_NAME/$VUE_APP_USER_NAME.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:$VUE_APP_USER_NAME/$VUE_APP_REPO_NAME.git main:gh-pages
+
+cd -
+
+$SHELL
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service');
 
+const REPO_NAME = process.env.VUE_APP_REPO_NAME || '/';
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' ? `/${REPO_NAME}/` : '/',
 });
