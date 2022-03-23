9aa9b88d8231df0fa019952ac28c94bd4fedea40
Evgenii <osipenkov.evgenii@gmail.com>
2022-03-23 12:25:27 +0300

    Add deployment to the github pages script

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 const { defineConfig } = require('@vue/cli-service');
 
+const REPO_NAME = process.env.VUE_APP_REPO_NAME || '/';
+console.log('REPO_NAME', REPO_NAME);
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' ? `/${REPO_NAME}/` : '/',
 });
